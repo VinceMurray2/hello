591b1068f815b6e920a9c20d90a865d34583c943
JCalvinHotz <jhotz@unl.edu>
2020-08-24 21:27:41 +0000

    both name and major

--- hello.c
+++ hello.c
@@ -1,17 +1,18 @@
 /**
  * Author Vincent Murray
  * Date 8/17/2020
- * 
+ *
  * A simple hello word program
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char argv) {
-     
+
      printf("Vince Murray!\n");
+     printf("Computer Science and Math!\n");
      printf("Calvin Hotz!\n");
-     
+
      return 0;
-     
+
  }
