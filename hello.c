9aebf49e118e4ee397de4e737cefa506a2f59ada
Vince Murray <vmurray2@huskers.unl.edu>
2020-08-24 15:44:02 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author Vincent Murray
+ * Date 8/17/2020
+ * 
+ * A simple hello word program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char argv) {
+     
+     printf("Hello world!\n");
+     
+     return 0;
+     
+ }
